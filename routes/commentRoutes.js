245853141921eb7464150d36553b22da6ca5a056
Nanobel <codingit7823@naver.com>
2025-02-28 09:45:02 +0900

    수정 삭제 조회 추가

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 const commentController = require('../controllers/commentController');
 
-// 답글 등록 라우트
+// 답글 등록
 router.post('/curatings/:curatingId/comments', commentController.createComment);
+
+// 답글 수정
+router.put('/comments/:commentId', commentController.updateComment);
+
+// 답글 삭제
+router.delete('/comments/:commentId', commentController.deleteComment);
+
+// 답글 목록 조회
+router.get('/curatings/:curatingId/comments', commentController.getComments);
 
 module.exports = router;
